ff06acecccf5d36a3970560613e3b5136d550d6d
dongkee.lee <eastflag@gmail.com>
2021-11-15 18:46:29 +0900

    question 공간 확보

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -2,11 +2,18 @@ import {Layout} from "antd";
 import {MyCanvas} from "./MyCanvas";
 import React from "react";
 
+import styles from './Main.module.scss';
+
 export const Main: React.FC = () => {
 
   return (
-    < >
-      <MyCanvas></MyCanvas>
-    </>
+    <div className={styles.container}>
+      <div className={styles.header}></div>
+      <div className={styles.body}>
+        <MyCanvas></MyCanvas>
+        <div className={styles.question}>question</div>
+      </div>
+      <div className={styles.footer}></div>
+    </div>
   );
 }
